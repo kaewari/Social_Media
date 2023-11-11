5ce39d95735fbc5e02ebdfb8ea6c2e5feae15dbd
kaewari <sonhoang236@gmail.com>
2023-11-11 22:35:48 +0700

    add retry if connection fail

--- BE/configs/redis.js
+++ BE/configs/redis.js
@@ -1,13 +1,18 @@
-const redis = require("ioredis");
-
-const client = redis.createClient({
+const Redis = require("ioredis");
+const maxDelay = 1000;
+const maxRetries = 5;
+const client = new Redis({
   port: process.env.REDIS_PORT,
   host: process.env.REDIS_HOST,
+  retryStrategy: (times) => {
+    if (times <= maxRetries) return times * maxDelay;
+    else return null;
+  },
 });
 client.on("connect", () => {
-  console.log("connected to redis successfully!");
+  console.log("Connected to redis successfully!");
 });
 client.on("error", (err) => {
-  console.log("error: " + err.message);
+  console.error("Redis error: " + err.message);
 });
 module.exports = client;
