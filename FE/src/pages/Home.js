21caf9130bae58f3772c6211b9b9b51988eb2c12
kaewari <sonhoang236@gmail.com>
2023-11-13 00:59:13 +0700

    update routes

--- FE/src/pages/Home.js
+++ FE/src/pages/Home.js
@@ -9,6 +9,9 @@ import Message from "./Message";
 import Profile from "./Profile/Profile";
 import Register from "./Register/Register";
 import Video from "./Video";
+import Group from "../components/Group";
+import Shop from "../components/Shop";
+import Notification from "../components/Notification";
 
 function Home() {
   const Layout = () => {
@@ -28,11 +31,9 @@ function Home() {
           <Route index element={<Index />} />
           <Route path="/friend" element={<Friend />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/message" element={<Message />} />
           <Route path="/video" element={<Video />} />
-          <Route path="/notification" element={<Video />} />
-          <Route path="/shop" element={<Video />} />
-          <Route path="/group" element={<Video />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/group" element={<Group />} />
           <Route path="/book-mark" element={<BookMark />} />
         </Route>
       </Routes>
